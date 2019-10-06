d1f4ed4ce7048e26f2be6f1c79da95ef293ff97a
Isaac Fisher <isaac.fisher@gmail.com>
2019-10-07 01:59:32 +0300

    Updates

---
+++ precache-manifest.4f46d2e33a32a95ecb5b308bc78d6198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f316d14fbb7c2f1576c6ae89fe6cf88c",
+    "url": "//isaac-fisher.github.io/karatensor/index.html"
+  },
+  {
+    "revision": "a4710a80b6a1fc5892a8",
+    "url": "//isaac-fisher.github.io/karatensor/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6a43a269b522532009bb",
+    "url": "//isaac-fisher.github.io/karatensor/static/js/2.fdf53ff0.chunk.js"
+  },
+  {
+    "revision": "a4710a80b6a1fc5892a8",
+    "url": "//isaac-fisher.github.io/karatensor/static/js/main.71cbfc3f.chunk.js"
+  },
+  {
+    "revision": "106595cf055f83bba44c",
+    "url": "//isaac-fisher.github.io/karatensor/static/js/runtime~main.2cb26354.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.966237ee0be1b090f6ed5523885df8a1.js"
+  "//isaac-fisher.github.io/karatensor/precache-manifest.4f46d2e33a32a95ecb5b308bc78d6198.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//isaac-fisher.github.io/karatensor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
