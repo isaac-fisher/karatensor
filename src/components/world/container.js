a950e9af90e0e34e1269f633aab2307efe1cf754
isaac-fisher <isaac.fisher@gmail.com>
2020-01-07 23:06:44 +0200

    split world

--- src/components/world/container.js
+++ src/components/world/container.js
@@ -2,47 +2,10 @@ import React, { useState } from 'react';
 import { useInterval } from '../../utils/setInterval';
 import styled from "styled-components";
 import { moves } from '../../utils/constants';
-import Player from './player';
+import Player from './objects/player';
+import WorldDisplayer from './displayer';
 
 const playerWidth = 40;
-const playerHeight = 100;
-
-const SceneWrapper = styled('div')`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-`;
-
-const SceneTop = styled('div')`
-  flex-grow: 1;
-  background-color: #007cf8;
-`
-
-const SceneCenter = styled('div')`
-  height: 40%;
-  width: 100%;
-  display: flex;
-  background-color: #111;
-`
-
-const SceneBottom = styled('div')`
-  width: 100%;
-  height: 60px;
-  background-image: linear-gradient(45deg, #8989c5 25%, transparent 25%), linear-gradient(-45deg, #8989c5 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #8989c5 75%), linear-gradient(-45deg, #54fcfc 75%, #8989c5 75%);
-  background-size: 10px 10px;
-  background-position: 0 0, 0 5px, 5px -5px, -5px 0px;
-`;
-
-const PlayerWrapper = styled('div')`
-  position: absolute;
-  bottom: 45px;
-  width: ${playerWidth}px;
-  height: ${playerHeight}px;
-  left: ${({location}) => location}px;
-  background-color: #aaa;
-`;
 
 function World({ move }) {
   
@@ -63,14 +26,7 @@ function World({ move }) {
   }, 80);
 
   return (
-    <SceneWrapper>
-      <SceneTop/>
-      <SceneCenter/>
-      <PlayerWrapper location={location}>
-        <Player/>
-      </PlayerWrapper>
-      <SceneBottom/>
-    </SceneWrapper>
+    <WorldDisplayer location={location}/>
   );
 }
 
