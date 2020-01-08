561a780075a724c40e13bdba3b7e7b0c94af414d
isaac-fisher <isaac.fisher@gmail.com>
2020-01-08 10:01:24 +0200

    small video + unselect button text

--- src/components/controllers/video.js
+++ src/components/controllers/video.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useInterval } from '../../utils/setInterval';
 import { moves } from '../../utils/constants';
+import styled from 'styled-components';
+
+const Button = styled('button')`
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; 
+`;
 
 function Vid({setMove}) {
   const classes = Object.values(moves)
@@ -51,10 +61,10 @@ function Vid({setMove}) {
   return (
     <div>
       {/*<button onClick={toggleStreaming}>{streaming ? 'Pause Video' :  'Start Video'}</button>*/}
-      {/* <video ref={videoEl} autoPlay width="227" height="227"/> */}
+      <div style={{ position: 'absolute', right: 0, top:0 }}><video ref={videoEl} autoPlay width="100" height="100"/></div>
       <div>
         {classes.map((name, index) => (
-          <button key={index} onMouseDown={() => setTraining(index)} onMouseLeave={()=> setTraining(null)} onMouseUp={()=> setTraining(null)}>{name} gesture</button>
+          <Button key={index} onMouseDown={() => setTraining(index)} onMouseLeave={()=> setTraining(null)} onMouseUp={()=> setTraining(null)}>{name} gesture</Button>
         ))}
         <br/>
         <button onClick={toggleTesting} style={{color: testing ? 'green' : 'red'}}>Test</button>
